f63968d57a4e13c62cf353f6843400b3f4ae0b4d
Norman Kluge <norman.kluge@hpi.de>
2018-04-10 16:09:38 +0200

    use schema validator for stgindexfile

--- src/main/java/de/uni_potsdam/hpi/asg/breeze2stg/io/stgindex/STGIndexFile.java
+++ src/main/java/de/uni_potsdam/hpi/asg/breeze2stg/io/stgindex/STGIndexFile.java
@@ -24,31 +24,63 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 public class STGIndexFile {
     private static final Logger logger = LogManager.getLogger();
 
     public static STGIndex readIn(File file) {
+        return internalReadIn(file, true);
+    }
+
+//    public static boolean isValidFile(File file) {
+//        return internalReadIn(file, false) != null;
+//    }
+
+    private static STGIndex internalReadIn(File file, boolean verbose) {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(STGIndex.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             if(file.exists()) {
+                JAXBContext jaxbContext = JAXBContext.newInstance(STGIndex.class);
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+                Schema schema = sf.newSchema(new StreamSource(STGIndexFile.class.getResourceAsStream("/" + STGIndexSchemaGenerator.filename)));
+                jaxbUnmarshaller.setSchema(schema);
                 STGIndex retVal = (STGIndex)jaxbUnmarshaller.unmarshal(file);
                 retVal.setProtocolDir(file.getParentFile());
                 return retVal;
             } else {
-                logger.error("File " + file.getAbsolutePath() + " not found");
+                if(verbose) {
+                    logger.error("File " + file.getAbsolutePath() + " not found");
+                }
                 return null;
             }
         } catch(JAXBException e) {
-            logger.error(e.getLocalizedMessage());
+            if(verbose) {
+                if(e.getLinkedException() instanceof SAXParseException) {
+                    SAXParseException e2 = (SAXParseException)e.getLinkedException();
+                    logger.error("File: " + file.getAbsolutePath() + ", Line: " + e2.getLineNumber() + ", Col: " + e2.getColumnNumber());
+                    logger.error(e2.getLocalizedMessage());
+                    return null;
+                }
+                logger.error(e.getLocalizedMessage());
+            }
+            return null;
+        } catch(SAXException e) {
+            if(verbose) {
+                logger.error(e.getLocalizedMessage());
+            }
             return null;
         }
     }
